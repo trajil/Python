185faaf09fb193b8443836da652e660a61b18e96
Trajil <trajil@web.de>
2024-01-12 16:11:13 +0100

    seperated code to art.py and wordlist.txt

--- GFN/LF05/hangman.py
+++ GFN/LF05/hangman.py
@@ -1,77 +1,8 @@
 import random
-
-# Step 3
-
-#TODO-1: - Use a while loop to let the user guess again. The loop should only stop once the user has guessed all the letters in the chosen_word and 'display' has no more blanks ("_"). Then you can tell the user they've won.
+from art import logo 
+from art import stages 
 
 game_on = True
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
-
-logo = ''' 
- _                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                   |___/    '''
 
 statement_right = "Right choice of a letter!"
 statement_wrong = "Wrong choice of a letter!"
@@ -84,221 +15,6 @@ statement_loser = "Sorry, you've lost."
 filler_length = 50
 filler = ":"
 
-word_list = [
-'abruptly', 
-'absurd', 
-'abyss', 
-'affix', 
-'askew', 
-'avenue', 
-'awkward', 
-'axiom', 
-'azure', 
-'bagpipes', 
-'bandwagon', 
-'banjo', 
-'bayou', 
-'beerkeeper', 
-'bikini', 
-'blitz', 
-'blizzard', 
-'boggle', 
-'bookworm', 
-'boxcar', 
-'boxful', 
-'buckaroo', 
-'buffalo', 
-'buffoon', 
-'buxom', 
-'buzzard', 
-'buzzing', 
-'buzzwords', 
-'caliph', 
-'cobweb', 
-'cockiness', 
-'croquet', 
-'crypt', 
-'curacao', 
-'cycle', 
-'daiquiri', 
-'dirndl', 
-'disavow', 
-'dizzying', 
-'duplex', 
-'dwarves', 
-'embezzle', 
-'equip', 
-'espionage', 
-'euouae', 
-'exodus', 
-'faking', 
-'fishhook', 
-'fixable', 
-'fjord', 
-'flapjack', 
-'flopping', 
-'fluffiness', 
-'flyby', 
-'foxglove', 
-'frazzled', 
-'frizzled', 
-'fuchsia', 
-'funny', 
-'gabby', 
-'galaxy', 
-'galvanize', 
-'gazebo', 
-'giaour', 
-'gizmo', 
-'glowworm', 
-'glyph', 
-'gnarly', 
-'gnostic', 
-'gossip', 
-'grogginess', 
-'haiku', 
-'haphazard', 
-'hyphen', 
-'iatrogenic', 
-'icebox', 
-'injury', 
-'ivory', 
-'ivy', 
-'jackpot', 
-'jaundice', 
-'jawbreaker', 
-'jaywalk', 
-'jazziest', 
-'jazzy', 
-'jelly', 
-'jigsaw', 
-'jinx', 
-'jiujitsu', 
-'jockey', 
-'jogging', 
-'joking', 
-'jovial', 
-'joyful', 
-'juicy', 
-'jukebox', 
-'jumbo', 
-'kayak', 
-'kazoo', 
-'keyhole', 
-'khaki', 
-'kilobyte', 
-'kiosk', 
-'kitsch', 
-'kiwifruit', 
-'klutz', 
-'knapsack', 
-'larynx', 
-'lengths', 
-'lucky', 
-'luxury', 
-'lymph', 
-'marquis', 
-'matrix', 
-'megahertz', 
-'microwave', 
-'mnemonic', 
-'mystify', 
-'naphtha', 
-'nightclub', 
-'nowadays', 
-'numbskull', 
-'nymph', 
-'onyx', 
-'ovary', 
-'oxidize', 
-'oxygen', 
-'pajama', 
-'peekaboo', 
-'phlegm', 
-'pixel', 
-'pizazz', 
-'pneumonia', 
-'polka', 
-'pshaw', 
-'psyche', 
-'puppy', 
-'puzzling', 
-'quartz', 
-'queue', 
-'quips', 
-'quixotic', 
-'quiz', 
-'quizzes', 
-'quorum', 
-'razzmatazz', 
-'rhubarb', 
-'rhythm', 
-'rickshaw', 
-'schnapps', 
-'scratch', 
-'shiv', 
-'snazzy', 
-'sphinx', 
-'spritz', 
-'squawk', 
-'staff', 
-'strength', 
-'strengths', 
-'stretch', 
-'stronghold', 
-'stymied', 
-'subway', 
-'swivel', 
-'syndrome', 
-'thriftless', 
-'thumbscrew', 
-'topaz', 
-'transcript', 
-'transgress', 
-'transplant', 
-'triphthong', 
-'twelfth', 
-'twelfths', 
-'unknown', 
-'unworthy', 
-'unzip', 
-'uptown', 
-'vaporize', 
-'vixen', 
-'vodka', 
-'voodoo', 
-'vortex', 
-'voyeurism', 
-'walkway', 
-'waltz', 
-'wave', 
-'wavy', 
-'waxy', 
-'wellspring', 
-'wheezy', 
-'whiskey', 
-'whizzing', 
-'whomever', 
-'wimpy', 
-'witchcraft', 
-'wizard', 
-'woozy', 
-'wristwatch', 
-'wyvern', 
-'xylophone', 
-'yachtsman', 
-'yippee', 
-'yoked', 
-'youthful', 
-'yummy', 
-'zephyr', 
-'zigzag', 
-'zigzagging', 
-'zilch', 
-'zipper', 
-'zodiac', 
-'zombie', 
-]
 
 def turn():
     global alive_current
@@ -346,8 +62,17 @@ def checkDisplay():
         game_finished = True
     
 while game_on:
+    
+    
+    # loading wordlist
+    with open("C:\Projekte\Python\GFN\LF05\wordlist.txt") as w:
+        lines = w.readlines()
+    word_list_txt = []
+    for line in lines:
+        word_list_txt.append(line.strip())
+                              
     print(logo)
-    chosen_word = random.choice(word_list)
+    chosen_word = random.choice(word_list_txt)
     print("Debug_mode: ",chosen_word)
     alive_current = len(stages) -1 
     #alive_current = len(chosen_word) + 5
