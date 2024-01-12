ac41110868917bc655aeeee6fae2146a1e70e5dc
Trajil <trajil@web.de>
2024-01-12 11:44:09 +0100

    Step 1 + comments

--- GFN/LF05/hangman.py
+++ GFN/LF05/hangman.py
@@ -1,5 +1,12 @@
 import random
 
+# Step 1
+# TODO-1 - Randomly choose a word from the word_list and assign it to a variable called chosen_word.
+
+# TODO-2 - Ask the user to guess a letter and assign their answer to a variable called guess. Make guess lowercase.
+
+# TODO-3 - Check if the letter the user guessed (guess) is one of the letters in the chosen_word.
+
 
 
 statement_right = "Right choice of a letter!"
